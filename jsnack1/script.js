bae9577005f90891e9861d03241d33fdb39f4f75
FraBif <francescobif@hotmail.it>
2023-05-18 16:10:36 +0200

    first snack done

---
+++ jsnack1/script.js
@@ -0,0 +1,6 @@
+let n = 10
+for (let i = 0; i < n; i++) { 
+  userNumbers = parseInt(prompt("inserisci un numero"))
+}
+
+console.log(userNumbers + userNumbers + userNumbers + userNumbers + userNumbers + userNumbers + userNumbers + userNumbers + userNumbers + userNumbers)
