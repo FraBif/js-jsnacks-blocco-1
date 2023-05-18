c0808f01d9eec685f5737154baf1ee40ceab5478
FraBif <francescobif@hotmail.it>
2023-05-18 17:26:00 +0200

    Snack 3 (rly) done

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -4,7 +4,7 @@ let n = 6
 for (let i = 0; i < n; i++) { 
   userNumbers = prompt("inserisci un numero")
   if (userNumbers % 2 !== 0) {
-    emptyBox.push(userNumber)
+    emptyBox.push(userNumbers)
     console.log(emptyBox)
     console.log("Hai riempito la scatola")
 } else {
